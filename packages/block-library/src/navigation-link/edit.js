2b93276c2dcf3942e96df24516717d5c96fdbddd
retrofox <rdsuarez@gmail.com>
2020-01-20 17:56:19 -0300

    navigation: store and populate inline colors It stores the color values for both text and background color in the Navigation block, and propagete these values to the Navigation Link block, in order to be able to apply colors using inline style.

--- packages/block-library/src/navigation-link/edit.js
+++ packages/block-library/src/navigation-link/edit.js
@@ -156,6 +156,11 @@ function NavigationLinkEdit( {
 						'has-background-color': navigationAttributes.backgroundColor,
 						[ `has-${ navigationAttributes.backgroundColor }-background-color` ]: !! navigationAttributes.backgroundColor,
 					} ) }
+				style={ {
+					borderColor: navigationAttributes.valueTextColor,
+					color: navigationAttributes.valueTextColor,
+					backgroundColor: navigationAttributes.valueBackgroundColor,
+				} }
 			>
 				<div>
 					<RichText
