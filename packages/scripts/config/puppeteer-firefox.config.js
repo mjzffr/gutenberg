db56d46e88cff597db40218b9a0e4335224184bd
Maja Frydrychowicz <mjzffr@gmail.com>
2020-12-24 15:47:40 -0500

    Run e2e tests against Firefox Nightly

---
+++ packages/scripts/config/puppeteer-firefox.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	launch: {
+		devtools: process.env.PUPPETEER_DEVTOOLS === 'true',
+		headless: process.env.PUPPETEER_HEADLESS !== 'false',
+		slowMo: parseInt( process.env.PUPPETEER_SLOWMO, 10 ) || 0,
+		// set the binary path here since puppeteer-core ignores Puppeteer's
+		// environment variables
+		executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+		product: 'firefox',
+		// Gutenberg depends on Puppeteer 3, so bring Firefox prefs up-to-date
+		extraPrefsFirefox: {
+			// Enable additional Firefox logging from its protocol implementation
+			// 'remote.log.level': 'Trace',
+			'browser.newtabpage.activity-stream.feeds.system.topstories': false,
+			'fission.autostart': false,
+		},
+	},
+};
